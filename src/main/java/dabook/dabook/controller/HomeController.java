ce34a7acc60895671931e4f0468a3d736719a3cb
minjichoi <wjdtjs3558@naver.com>
2024-01-07 03:46:45 +0900

    front

--- src/main/java/dabook/dabook/controller/HomeController.java
+++ src/main/java/dabook/dabook/controller/HomeController.java
@@ -1,7 +1,5 @@
 package dabook.dabook.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
